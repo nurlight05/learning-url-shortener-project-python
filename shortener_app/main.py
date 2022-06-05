78c486bd7f604227d422a9d3085a6e621484dfdc
nurlight05 <nurlight05@gmail.com>
2022-06-05 23:48:04 +0500

    Venv packages uploaded. Schemas & Database settings added.

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,20 @@
+from fastapi import HTTPException
+import validators
 from fastapi import FastAPI
 
+from . import schemas
+
 app = FastAPI()
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
 
 @app.get("/")
 def read_root():
     return "Welcome"
+
+@app.post("/url")
+def create_url(url: schemas.URLBase):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    return f"TODO: Create database entry for: {url.target_url}"
